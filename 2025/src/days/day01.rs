14906ff6f4b778464dfe6f646a572e53cf1b0aa4
Izan Gil <66965250+SrIzan10@users.noreply.github.com>
2025-12-01 08:18:19 +0100

    chore: d01 progress

--- 2025/src/days/day01.rs
+++ 2025/src/days/day01.rs
@@ -6,12 +6,53 @@ pub fn run() {
     println!("Part 2: {}", part2(&input));
 }
 
-fn part1(_input: &str) -> String {
-    // TODO: Solve part 1
-    "Not implemented".to_string()
+fn part1(input: &str) -> String {
+    let mut number = 50;
+    let mut solution = 0;
+    for imp in input.lines() {
+        let c = imp.chars().next().unwrap_or(' ');
+        let nums = &imp[1..].parse().unwrap();
+        match c {
+            'R' => {
+                number += nums;
+                if number == 99 {
+                    println!("holy shitasd f");
+                    solution += 1
+                } else if number < 0 {
+                    number += 99;
+                    println!("summing {}", number)
+                }
+
+                if number == 0 {
+                    println!("holy shit");
+                    solution += 1
+                }
+                println!("{}", number);
+            },
+            'L' => {
+                number -= nums;
+                if number == 100 {
+                    println!("holy shitasd f");
+                    solution += 1
+                } else if number < 0 {
+                    number += 100;
+                    println!("summing {}", number)
+                }
+
+                if number == 0 || number == 100 {
+                    println!("holy shit");
+                    solution += 1;
+                    number = 0;
+                }
+                println!("{}", number); 
+            },
+            _ => {}
+        }
+    }
+    return solution.to_string();
 }
 
 fn part2(_input: &str) -> String {
     // TODO: Solve part 2
-    "Not implemented".to_string()
+    return "Not implemented".to_string()
 }
